5517b89d267e4a11fc279874eef090dc087f0991
Maximiliano Ezequiel Rios <maximrios@gmail.com>
2014-03-25 17:08:59 -0300

    Actualizando

--- application/models/rosobe/categorias_model.php
+++ application/models/rosobe/categorias_model.php
@@ -41,14 +41,24 @@ class Categorias_model extends CI_Model {
         $result = $this->db->query($sql, array($id))->result_array();
         return $result[0]['result'];
     }
-    public function dropdownProductos() {
-        $sql = 'SELECT * FROM diario_view_productos';
-        $query = $this->db->query($sql)->result();
-        $subgrupos[0] = 'Seleccione un producto ...';
-        foreach($query as $row) {
-            $subgrupos[$row->idProducto] = $row->nombreProducto;
-        }
-        return $subgrupos;
+    
+    public function obtenerCategoriasProducto($idProducto = 0) {
+        $sql = 'SELECT c.idCategoria, c.nombreCategoria, IF(cp.idCategoriaProducto IS NULL, 0, 1) AS checked, cp.idProducto
+                FROM rosobe_categorias c
+                LEFT JOIN rosobe_categorias_productos cp ON c.idCategoria = cp.idCategoria AND cp.idProducto = ?;';
+        return $this->db->query($sql, (int) $idProducto)->result_array();
+    }
+
+    public function eliminarCategoriasProducto($idProducto) {
+        $sql = 'DELETE FROM rosobe_categorias_productos WHERE idProducto = ?;';
+        $this->db->query($sql, (int) $idProducto);
+        return $this->db->affected_rows();
+    }
+
+    public function guardarCategoriasProducto($aParms) {
+        $sql = 'INSERT INTO rosobe_categorias_productos(idCategoria, idProducto) VALUES (?, ?);';
+        $this->db->query($sql, $aParms);
+        return $this->db->affected_rows();
     }
 }
 
